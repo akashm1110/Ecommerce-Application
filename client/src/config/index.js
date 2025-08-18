36baa1aa9b18a3967b2dca8ebe5ff4e03fb4e067
akashm1110 <akashsanjay1626@gmail.com>
2025-08-19 01:59:30 +0530

    search change

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -139,11 +139,11 @@ export const registerFormControls = [
       label: "Accessories",
       path: "/shop/listing",
     },
-    // {
-    //   id: "search",
-    //   label: "Search",
-    //   path: "/shop/search",
-    // },
+    {
+      id: "search",
+      label: "Search",
+      path: "/shop/search",
+    },
   ];
 
   export const categoryOptionsMap = {
